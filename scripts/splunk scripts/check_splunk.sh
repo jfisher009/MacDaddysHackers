b71872e3b012a5ddf113d21efbe9336a3886a11b
Sam <60146956+srudqvist@users.noreply.github.com>
2022-11-12 04:46:45 +0100

    Update check_splunk.sh

--- scripts/splunk scripts/check_splunk.sh
+++ scripts/splunk scripts/check_splunk.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Check if Splunk is installed
 if [ -f "/opt/splunk/bin/splunk" ]; then
-    echo "[$(date +%H:%M:%S)]: Splunk is already installed"
+    echo "[$(date +%H:%M:%S)]: Splunk is already installed in /opt/splunk/bin"
   else
     echo "[$(date +%H:%M:%S)]: Splunk is NOT installed"
+if [ -f "/opt/splunkforwarder/bin/splunk" ]; then
+    echo "[$(date +%H:%M:%S)]: Splunk forwarder is already installed"
 fi
