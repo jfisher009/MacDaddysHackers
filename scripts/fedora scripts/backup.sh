593ef798304e0324fda3519e8274f2a4b9068475
edbratkov <60157459+edbratkov@users.noreply.github.com>
2022-11-11 20:25:18 -0600

    Update backup.sh

--- scripts/fedora scripts/backup.sh
+++ scripts/fedora scripts/backup.sh
@@ -4,12 +4,12 @@ mkdir /etc/backups/
 mkdir /etc/backups/dovecot
 mkdir /etc/backups/dovecot/conf.d
 
-cp /etc/dovecot/* /etc/backups/dovecot
-cp /etc/dovecot/conf.d/* /etc/backups/dovecot/conf.d
+cp -r /etc/dovecot/* /etc/backups/dovecot
+cp -r /etc/dovecot/conf.d/* /etc/backups/dovecot/conf.d
 
 
 mkdir /etc/backups/postfix
-cp /etc/postfix/* /etc/backups/postfix
+cp -r /etc/postfix/* /etc/backups/postfix
 
 mkdir /etc/backups/chronyd
-cp /etc/chrony.* /etc/backups/chronyd
+cp -r /etc/chrony.* /etc/backups/chronyd
