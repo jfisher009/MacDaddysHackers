1caedf1e4ab361c6b1c8e484a433823bcd5e19a9
mmakarov10 <91093217+mmakarov10@users.noreply.github.com>
2023-02-16 17:55:20 -0600

    Update iptables.sh

--- scripts/centos scripts/iptables.sh
+++ scripts/centos scripts/iptables.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
+iptables -F
+iptables -X
 
-# Allow incoming traffic on port 80 (HTTP)
-iptables -A INPUT -p tcp --sport 80 -j ACCEPT
+# Set default policy
+iptables -P INPUT DROP
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD DROP
 
-# Allow incoming traffic on port 443 (HTTPS)
-iptables -A INPUT -p tcp --sport 443 -j ACCEPT
+# Allow incoming web traffic (HTTP and HTTPS)
+iptables -A INPUT -i ens192 -p tcp --sport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o ens192 -p tcp --dport 80 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -i ens192 -p tcp --sport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o ens192 -p tcp --dport 443 -m state --state ESTABLISHED -j ACCEPT
 
-# Allow incoming DNS traffic on port 53
-iptables -A INPUT -p udp --sport 53 -j ACCEPT
+
+# Allow SSH traffic on port 64435
+iptables -A INPUT -i ens192 -p tcp --sport 64435 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o ens192 -p tcp --dport 64435 -m state --state ESTABLISHED -j ACCEPT
+
+# Allow incoming DNS traffic
+iptables -A INPUT -i ens192 -p udp --sport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o ens192 -p udp --dport 53 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -i ens192 -p tcp --sport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o ens192 -p tcp --dport 53 -m state --state ESTABLISHED -j ACCEPT
 
 # Allow incoming ICMP traffic
-iptables -A INPUT -p icmp -j ACCEPT
+iptables -A INPUT -i ens192 -p icmp -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o ens192 -p icmp -m state --state ESTABLISHED -j ACCEPT
 
-# Drop all other incoming traffic
-iptables -A INPUT -j DROP
 
 
