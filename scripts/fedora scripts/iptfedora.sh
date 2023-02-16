f79010e1c36df7e8ca8883caa3fa54535e2273cb
edbratkov <60157459+edbratkov@users.noreply.github.com>
2023-02-16 16:42:12 -0600

    Update iptfedora.sh

--- scripts/fedora scripts/iptfedora.sh
+++ scripts/fedora scripts/iptfedora.sh
@@ -1,21 +1,29 @@
 
 #!/bin/bash
-# Flush all existing rules
+# Flush all existing rules and chains
 iptables -F
+iptables -X
 
-# Allow traffic on ports 25, 587, 143, 993, 110 and 995
-iptables -A INPUT -p tcp --dport 25 -j ACCEPT
-iptables -A INPUT -p tcp --dport 587 -j ACCEPT
-iptables -A INPUT -p tcp --dport 143 -j ACCEPT
-iptables -A INPUT -p tcp --dport 993 -j ACCEPT
-iptables -A INPUT -p tcp --dport 110 -j ACCEPT
-iptables -A INPUT -p tcp --dport 995 -j ACCEPT
+# Set default polcicy
+iptables -P INPUT DROP
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD DROP
+
+# Allow traffic on ports 25, 143, 110, 80, 443, 53
+iptables -A INPUT -p tcp --sport 25 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --sport 143 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --sport 110 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --sport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --sport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --sport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
+
 
 # Allow traffic for responses to established connections
 iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# Reject all other outgoing traffic
-iptables -A OUTPUT -j REJECT
+# Allow incoming ICMP traffic
+iptables -A INPUT -p icmp -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p icmp -m state --state ESTABLISHED -j ACCEPT
 
 # Block all other incoming traffic
 iptables -A INPUT -j DROP
