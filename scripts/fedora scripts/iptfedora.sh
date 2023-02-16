19fea11424e6d6e46af498f7c4f875cd828cc67c
edbratkov <60157459+edbratkov@users.noreply.github.com>
2023-02-16 16:51:36 -0600

    Update iptfedora.sh

--- scripts/fedora scripts/iptfedora.sh
+++ scripts/fedora scripts/iptfedora.sh
@@ -1,29 +1,29 @@
-
 #!/bin/bash
-# Flush all existing rules and chains
-iptables -F
-iptables -X
 
-# Set default polcicy
+# Flush existing rules
+iptables -F
+
+# Set default policy
 iptables -P INPUT DROP
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD DROP
 
-# Allow traffic on ports 25, 143, 110, 80, 443, 53
-iptables -A INPUT -p tcp --sport 25 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp --sport 143 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp --sport 110 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp --sport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp --sport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp --sport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
+# Allow incoming web traffic (HTTP and HTTPS)
+iptables -A INPUT -i ens192 -p tcp --sport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o ens192 -p tcp --dport 80 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -i ens192 -p tcp --sport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o ens192 -p tcp --dport 443 -m state --state ESTABLISHED -j ACCEPT
 
+# Allow SSH traffic on port 64435
+iptables -A INPUT -i ens192 -p tcp --sport 64435 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o ens192 -p tcp --dport 64435 -m state --state ESTABLISHED -j ACCEPT
 
-# Allow traffic for responses to established connections
-iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+# Allow incoming DNS traffic
+iptables -A INPUT -i ens192 -p udp --sport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o ens192 -p udp --dport 53 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -i ens192 -p tcp --sport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o ens192 -p tcp --dport 53 -m state --state ESTABLISHED -j ACCEPT
 
 # Allow incoming ICMP traffic
-iptables -A INPUT -p icmp -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p icmp -m state --state ESTABLISHED -j ACCEPT
-
-# Block all other incoming traffic
-iptables -A INPUT -j DROP
+iptables -A INPUT -i ens192 -p icmp -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o ens192 -p icmp -m state --state ESTABLISHED -j ACCEPT
