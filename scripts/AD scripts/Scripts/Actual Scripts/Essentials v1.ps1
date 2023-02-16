3153b12f9187d9860e875f2e9e2d45265559d6e1
CallSignTater <93746681+CallSignTater@users.noreply.github.com>
2023-02-15 23:27:59 -0800

    Add files via upload

---
+++ scripts/AD scripts/Scripts/Actual Scripts/Essentials v1.ps1
@@ -0,0 +1,18 @@
+﻿#Stop and disable printspooler service
+Stop-Service -name Spooler -force
+Set-Service -name Spooler -StartupType disabled
+#Confirm status of printspooler
+Get-Service -name Spooler
+
+#Set inbound firewall rules
+New-NetFirewallRule -DisplayName "Initial Block" `
+-Direction Inbound `
+-LocalPort 135,139,445,49152-49155 `
+-Protocol TCP `
+-Action Block
+#Set outbound firewall rules
+New-NetFirewallRule -DisplayName "Initial Block" `
+-Direction Outbound `
+-LocalPort 135,139,445,49152-49155 `
+-Protocol TCP `
+-Action Block
