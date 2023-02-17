621a7db247b6644c9e80365e6f11f05278b48219
CallSignTater <93746681+CallSignTater@users.noreply.github.com>
2023-02-16 22:44:34 -0600

    Update NTP setup.ps1

--- scripts/AD scripts/Scripts/NTP setup.ps1
+++ scripts/AD scripts/Scripts/NTP setup.ps1
@@ -1,6 +1,14 @@
-﻿net start w32time
+#make sure windows time service is running
+net start w32time
+#configure windows time service (NTP), change the manualpeerlist value to the NTP source. If debian, use it's internal IP.
 w32tm /config /manualpeerlist:time.nist.gov /syncfromflags:manual /reliable:yes /update
+#restart the service
 net stop w32time
 net start w32time
-w32tm /monitor
-w32tm /query /status
+#check peers to make sure it is active
+w32tm /query /peers
+#check source to make sure proper source is listed (NOT Local CMOS Clock)
+w32tm /query /source
+
+#keep window open until finished reviewing
+Read-Host -Prompt "Finished, Press Enter to exit"
